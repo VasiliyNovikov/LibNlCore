b14f5aa190f084e48ffe912c5a4dc9c7fdcb86c3
Vasyl Novikov <vanoviko@microsoft.com>
2026-02-16 15:54:37 +0000

    Unused code

--- NetlinkCore/Interop/Generic/GENL_ETHTOOL_A_BITSET_BIT.cs
+++
@@ -1,9 +0,0 @@
-namespace NetlinkCore.Interop.Generic;
-
-internal enum GENL_ETHTOOL_A_BITSET_BIT : ushort
-{
-    ETHTOOL_A_BITSET_BIT_UNSPEC,
-    ETHTOOL_A_BITSET_BIT_INDEX,  /* u32 */
-    ETHTOOL_A_BITSET_BIT_NAME,   /* string */
-    ETHTOOL_A_BITSET_BIT_VALUE,  /* flag */
-}
--- NetlinkCore/Interop/Generic/GENL_ETHTOOL_A_BITSET_BITS_BIT.cs
+++
@@ -1,7 +0,0 @@
-namespace NetlinkCore.Interop.Generic;
-
-internal enum GENL_ETHTOOL_A_BITSET_BITS_BIT : ushort
-{
-    ETHTOOL_A_BITSET_BITS_UNSPEC,
-    ETHTOOL_A_BITSET_BITS_BIT, /* nest - _A_BITSET_BIT_* */
-}
